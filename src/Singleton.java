55d1775e02dedd279887d4c89a31be5b28f80f9e
Tim Degold <tdegold@student.tgm.ac.at>
2020-01-22 11:19:20 +0100

    finished up

--- src/Singleton.java
+++ src/Singleton.java
@@ -2,7 +2,7 @@ public class Singleton {
 
     private static Singleton instance;
 
-    public String testString = "this test is used to test the singleton design pattern";
+    public String testString = "this text is used to test the singleton design pattern";
 
     private Singleton(){}                           // private Constructor
 
